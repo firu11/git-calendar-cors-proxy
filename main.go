610077a39f7cea1ef225b64559a2ee215f8944ba
firu11 <firu@firu.dev>
2026-01-07 16:50:05 +0100

    basic implementation

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", proxyHandler)
+
+	s := &http.Server{
+		Addr:           ":8000",
+		Handler:        accessLog(mux),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	// JSON logging
+	// logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	slog.SetDefault(logger)
+
+	slog.Info("Cors Proxy is running on " + s.Addr)
+	log.Fatal(s.ListenAndServe())
+}
+
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	// add cors header to allow any browser to use this endpoint
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// get the destination url from query
+	destUrlQuery := r.URL.Query().Get("url")
+	if destUrlQuery == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Url query param is missing\n")
+		return
+	}
+	destUrl, err := url.ParseRequestURI(destUrlQuery)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Url query param is invalid\n")
+		return
+	}
+
+	// prepare the request to destination
+	req, err := http.NewRequestWithContext(r.Context(), r.Method, destUrl.String(), r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		slog.Error(err.Error())
+		return
+	}
+	copyHeaders(r.Header, req.Header)
+	removeHopByHopHeaders(req.Header)
+
+	// send the actual request
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	// forward the headers and body back to client
+	removeHopByHopHeaders(resp.Header)
+	copyHeaders(resp.Header, w.Header())
+
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+}
